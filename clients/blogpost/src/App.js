b3f399461c7a1cc62593f27135af1a39521a5bdb
armaan <karmankkhan@gmail.com>
2022-09-22 23:22:39 +0530

    navbar done

--- clients/blogpost/src/App.js
+++ clients/blogpost/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import Post from "./Components/Post";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold text-center">App</h1>
-      App
+      <Navbar />
+      <Post />
     </div>
   );
 }
